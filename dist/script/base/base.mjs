1bdbba9186cd29690cdca8753c552b46cd540ce5
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-02-26 16:38:42 +0100

    test value from variables.mjs file

--- dist/script/base/base.mjs
+++ dist/script/base/base.mjs
@@ -1,5 +1,9 @@
 
 
-import { API_URL } from './variable.mjs';
+import { API_URL, metode_API } from './variable.mjs';
 
+
+console.log('API URL');
 console.log(API_URL)
+console.log('Metod for api');
+console.log(metode_API)
