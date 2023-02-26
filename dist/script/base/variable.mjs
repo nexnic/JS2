948e2cabb0ada593dcff987faf5924cb6d49c758
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-02-26 16:15:51 +0100

    start on api url value

---
+++ dist/script/base/variable.mjs
@@ -0,0 +1,17 @@
+/*
+* 
+* a variable is a container that stores a value, which can be a string, number, boolean, or other data type.
+* Use strice mode is one
+* strict mode is a feature that enables a stricter version of JavaScript, which helps to prevent common mistakes and improve code quality.
+*/
+
+'use strict';
+
+
+export const API_URL = {
+    BASE_URL: 'https://nf-api.onrender.com',
+    register_URL: '/api/v1/social/auth/register',
+    login_URL: '/api/v1/social/auth/login',
+    post_URL: '/api/v1/social/posts'
+}
+
