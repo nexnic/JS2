e7b6bcb29f2c28ce0de1eeebfb598274029a5d34
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-02-28 13:24:51 +0100

    adding btn for register and forgot password

--- dist/script/base/domcontroller.mjs
+++ dist/script/base/domcontroller.mjs
@@ -15,6 +15,10 @@
             export const inputPWD = document.querySelector('#login_password');
         // Submit btn 
             export const btnLoginSubmit = document.querySelector('#login_submit');
+        // Register user btn 
+            export const btnregister = document.querySelector('#btn_register');
+        // Forgot Password Btn 
+            export const forgotPWD =  document.querySelector('#btn_forgotPWD');
 
 // footer 
 
