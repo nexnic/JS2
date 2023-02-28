9625d1e880a1ee2ab9c37cfea4cebb4902371bb3
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-02-28 13:21:40 +0100

    adding login form for export

---
+++ dist/script/base/domcontroller.mjs
@@ -0,0 +1,20 @@
+
+
+// dom Tree 
+
+// Befor Header 
+
+// header
+
+// main 
+
+    // Logg in form 
+        // Email Form  
+            export const inputUserName = document.querySelector('#login_email');
+        // Password form 
+            export const inputPWD = document.querySelector('#login_password');
+        // Submit btn 
+            export const btnLoginSubmit = document.querySelector('#login_submit');
+
+// footer 
+
