1801d4c6571b376a07b543ee5483a7d5acf927f4
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-02-28 17:14:43 +0100

    adding script for form

---
+++ dist/script/components/form.mjs
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+
+
+
+
+
+//inputUserName.addEventListener('input', check_login_mail);
+
+export function check_login_mail(e) {
+    console.log(e.target.value);
+}
+
+export function check_login_password(e){
+    console.log(e.target.value)
+}
+
+export function check_login_form(e) {
+    
+}
