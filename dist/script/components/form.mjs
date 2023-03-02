fbd413ffaf2dc6a88b8ba309a3ff361b3d5ae617
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-02 10:37:19 +0100

    adding before newvaliedator for email

--- dist/script/components/form.mjs
+++ dist/script/components/form.mjs
@@ -1,22 +1,38 @@
-/**
- * 
- */
+import {formsn , formsnEmail, formsnPassword} from '../base/domcontroller.mjs';
+import { filterREG } from '../base/variable.mjs';
+import { fetchAPI } from './API.mjs'
 
+formsn.addEventListener('submit', (event) => {
+    let email = formsn.elements['email'].value 
+    let _password = formsn.elements['password'].value
 
+    let check = validEmail(email);
+    if(check) {
+        fetchAPI(email, _password)
+    } else {
+        
+    }
+    event.preventDefault();
+})
 
+function validEmail(value1) {
+    let email = value1
+    let splitemail = email.split('@');
+    let Domain = splitemail[1];
+    let Reg = /\S+@\S+\.\S+/; 
+    let checkMail = Reg.test(email);
 
-
-
-//inputUserName.addEventListener('input', check_login_mail);
-
-export function check_login_mail(e) {
-    console.log(e.target.value);
+    filterREG.find(({ item })=> {
+        if(Domain && checkMail) {
+            return true
+        }
+        else{
+            return false 
+        }
+    }) 
 }
 
-export function check_login_password(e){
-    console.log(e.target.value)
-}
+function validPWD(value1) {
+    let _password = value1
 
-export function check_login_form(e) {
-    
 }
