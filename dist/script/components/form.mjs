c1e73cf0e691393c6ef532eb483ffd70583b83ed
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-02 10:47:51 +0100

    adding new emailvaliedator

--- dist/script/components/form.mjs
+++ dist/script/components/form.mjs
@@ -6,14 +6,24 @@ formsn.addEventListener('submit', (event) => {
     let email = formsn.elements['email'].value 
     let _password = formsn.elements['password'].value
 
-    let check = validEmail(email);
+    let check = NewValidEmail(email);
     if(check) {
-        fetchAPI(email, _password)
+        
     } else {
         
     }
     event.preventDefault();
 })
+
+// Test new metod 
+function NewValidEmail(value1) {
+    let email =  value1;
+    const regex = new RegExp('[a-z0-9]+@stud.noroff.no');
+
+    let checkEmail =  regex.test(email);
+    
+    return checkEmail 
+}
 
 function validEmail(value1) {
     let email = value1
@@ -33,6 +43,9 @@ function validEmail(value1) {
 }
 
 function validPWD(value1) {
-    let _password = value1
+    let _password = value1; 
 
+    let Strongreg = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})');
+    
+    let checkPWD = Strongreg.test(_password);
 }
