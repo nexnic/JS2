79ee57488b7b4f17d91b0c6c499adce12c04dc6a
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-02 15:30:50 +0100

    adding login metod

--- dist/script/components/methods.mjs
+++ dist/script/components/methods.mjs
@@ -1,2 +1,32 @@
 
 
+let MetodData = {
+    method: '2',
+    headers: {
+        'Content-Type' : 'application/json',
+    }
+}
+
+    let email =  'test_user_demo@noroff.no'
+    let password = 'testtest1234'
+
+    let userData = {
+        email: '',
+        password: '',
+    };
+
+// POST 
+    function metodPOST(value1, value2){
+        let postData = MetodData;
+        let tempUser = userData;
+        postData['method'] = 'POST'
+        tempUser['email'] = value1
+        tempUser['password'] = value2
+        postData['body'] = JSON.stringify(tempUser);
+        console.log(postData)
+
+        
+        
+    }
+
+    metodPOST(email, password)
