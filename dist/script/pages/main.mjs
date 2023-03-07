ca6842c96c02f76cefb58bcd1a86f090b397fb6a
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-07 15:13:33 +0100

    small update
    Need to remove some thing

--- dist/script/pages/main.mjs
+++ dist/script/pages/main.mjs
@@ -3,11 +3,3 @@ import {MethodToken} from '../components/methods.mjs';
 import {CheckAfterPost , ShowFeed} from '../components/post.mjs';
 import {fetchtest} from '../components/fetch.mjs';
 
-const test =  fetchtest(3,'post');
-
-
-let Check = CheckAfterPost()
-if(Check){
-    
-    ShowFeed()
-}
