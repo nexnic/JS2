3f4984063e5281a708dd6ed0694cb3890eea493b
Kent Erik Hole <77827255+nexnic@users.noreply.github.com>
2023-03-08 15:03:14 +0100

    Delete main.mjs
    need to remove this for conflit

--- dist/script/pages/main.mjs
+++
@@ -1,5 +0,0 @@
-import {Validlogin} from '../components/validator.mjs';
-import {MethodToken} from '../components/methods.mjs';
-import {CheckAfterPost , ShowFeed} from '../components/post.mjs';
-import {fetchtest} from '../components/fetch.mjs';
-
