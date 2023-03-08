c72e1a17f3b99afd0ae3027dbc3dfdde30200026
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-08 14:56:28 +0100

    Create tools.mjs
    This will have small function

---
+++ dist/script/components/tools.mjs
@@ -0,0 +1,18 @@
+
+
+export function Redirect(value1){
+    window.location.href = `${value1}`;
+}
+
+export function StorageRemove(value1) {
+    localStorage.removeItem(`${value1}`);
+}
+
+export function cleanLocalStorage() {
+    localStorage.clear()
+    if(localStorage){
+        return true
+    }else {
+        return false
+    }
+}
