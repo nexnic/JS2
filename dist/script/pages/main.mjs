b3d2b76461977d3ede0c1ae72712dcc386a21058
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-08 14:57:35 +0100

    Update main.mjs
    add more metode to main page

--- dist/script/pages/main.mjs
+++ dist/script/pages/main.mjs
@@ -1,13 +1,12 @@
-import {Validlogin} from '../components/validator.mjs';
-import {MethodToken} from '../components/methods.mjs';
-import {CheckAfterPost , ShowFeed} from '../components/post.mjs';
-import {fetchtest} from '../components/fetch.mjs';
+// BASE
+    // Dom Controller 
+        import {navProfileImage} from '../base/domcontroller.mjs';
+// Components
+    // Nav
+        import {BuildNav} from '../components/nav.mjs';
 
-const test =  fetchtest(3,'post');
 
+        
 
-let Check = CheckAfterPost()
-if(Check){
-    
-    ShowFeed()
-}
+// Build nav 
+BuildNav()
