b9a2ea831861d81154acd7333280ac870a5491df
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-08 14:51:38 +0100

    Update base.mjs
    Start on new function

---
+++ dist/script/base/base.mjs
@@ -0,0 +1,30 @@
+// Import file 
+    // BASE 
+        // Dom Controller 
+            import { btnProfile, btnSettings, btnLogout, formsmSearch, Searchinput } from './domcontroller.mjs';
+    // Components 
+        // Button 
+            import {Profilebtn, signoutbtn} from '../components/button.mjs';
+        // Navigation
+        // Fetch 
+            import {fetchToken} from '../components/fetch.mjs';
+
+
+
+
+// Event Lostener
+
+btnProfile.addEventListener('click', (event)=> Profilebtn())
+
+btnSettings.addEventListener('click', (event) => Profilebtn())
+
+btnLogout.addEventListener('click', (event) => signoutbtn())
+
+formsmSearch.addEventListener("keypress", function(event) {
+    if(event.key === 'Enter') {
+        let data = formsmSearch.elements['Search'].value
+        console.log(data);
+        event.preventDefault();
+    }
+    
+})
