a878c95957301eea91e43c285fce1f688c3cd0c5
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-08 15:13:29 +0100

    Update
    Update

---
+++ dist/script/pages/main.mjs
@@ -0,0 +1,12 @@
+// BASE
+    // Dom Controller 
+        import {navProfileImage} from '../base/domcontroller.mjs';
+// Components
+    // Nav
+        import {BuildNav} from '../components/nav.mjs';
+//
+
+        
+
+// Build nav 
+BuildNav()
