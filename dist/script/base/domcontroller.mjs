f13cafb67a60a169a302398eea68c11d477286ca
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-11 13:36:00 +0100

    Update domcontroller.mjs
    Add form and input for form

--- dist/script/base/domcontroller.mjs
+++ dist/script/base/domcontroller.mjs
@@ -6,17 +6,27 @@
 
 // header
 
-// main 
-
-    // Sign In form 
-        // Form 
-           export const formsn = document.querySelector('#signin');
-        // Email Input  
-            export const formsnEmail = document.querySelector('#signin_email');
-        // Password 
-            export const formsnPassword = document.querySelector('#signin_password');
-        // Submit Button 
-            export const formsnbtn = document.querySelector('#s')
+// Index HTML 
+    // Form 
+        // Sign In form
+            export const signform = document.querySelector('#form-signin');
+            // Form Email 
+                export const signEmail = document.querySelector('#form-signin__email');
+            // Form Password
+                export const signPws = document.querySelector('#form-signin__password')
+        // Sign up form
+            export const signupform = document.querySelector('#form-signup');
+            // Form Name 
+                export const signupName =document.querySelector('#form-signup__name');
+            // From Email   
+                export const signupEmail = document.querySelector('#form-signup__email');
+            // Form Password
+                export const signupPWS = document.querySelector('#form-signup_password');
+            // Form Banner 
+                export const signupBanner = document.querySelector('#form-signup-banner');
+            // Form Avatar 
+                export const signupAvatar = document.querySelector('#form-signup-avatar');
+            
 // footer 
 
 // Main Page 
