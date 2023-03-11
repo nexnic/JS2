bc2ae18dd9d7a6893dac243fd31846471cfc365b
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-03-11 14:48:01 +0100

    Create Error.msj
    This is function for error.mjs

---
+++ dist/script/components/error.mjs
@@ -0,0 +1,21 @@
+import {ErrorText} from '../base/variable.mjs';
+import {errorBox} from '../base/domcontroller.mjs';
+
+/**
+ * 
+ * @param {*id} value1
+ */
+export function ErrorMSg(value1) {
+    let msg = ErrorText[value1];
+    errorBox.innerHTML += `
+        <p>
+            ${msg}
+        </p>
+    `
+    errorBox.classList.add('alert-warning');
+
+    setTimeout(() => {
+        errorBox.classList.remove('alert-warning');
+    }, 3000);
+}
+
