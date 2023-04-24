1d077f5ef935d7b0499f4782150263fdc147abe5
Kent Erik <kent.erik.hole@outlook.com>
2023-04-24 12:55:39 +0200

    fiks login

--- dist/script/base/variable.mjs
+++ dist/script/base/variable.mjs
@@ -25,7 +25,7 @@ export const apiID = {
 
 
 export const ErrorText = {
-    1: 'The email address you provided is not valid. Please ensure that your email address contains either "@domain.com" or "@doma.com" in order to proceed. Kindly check and correct the email address provided. Thank you.', 
+    1: 'The email address you provided is not valid. Please ensure that your email address contains either "@stud.noroff.no" or "@noroff.no" in order to proceed. Kindly check and correct the email address provided. Thank you.', 
     2: 'We are sorry, but there was an error processing your request through the API. If the problem persists, please contact our support team for further assistance. Thank you.',
     3: 'Your password must be at least 8 characters long.', 
     4: 'Name can only use a-Z, 0-9, and _',
