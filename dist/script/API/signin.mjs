179487a2fb0ec5cb0be65ac07f9cb17a78462683
Kent Erik <kent.erik.hole@outlook.com>
2023-04-24 12:56:30 +0200

    was fail on this

--- dist/script/API/signin.mjs
+++ dist/script/API/signin.mjs
@@ -2,14 +2,20 @@ import {findAPIID} from '../components/tools.mjs';
 
 
 export async function FetchSingin(value1, value2){
-    const url = findAPIID(value2)
-    const method =  value1
-    const fetchResult = await fetch(url, method)
-    const result  = await fetchResult.json()
-    if(fetchResult.ok){
+    
+    
+
+    try {
+        const url = findAPIID(value2)
+        const method =  value1
+        
+        const fetchResult = await fetch(url, method)
+        const result  = await fetchResult.json()
         
         return result
-        
+
+    } catch (error) {
+        console.log(error)
     }
     
     
